9baf308934f6ee3ffd60628bce1232b9e7f677ae
eric-guanwy <guanwy01@gmail.com>
2018-05-26 12:49:01 +0800

    pwdreset email debug logger added

--- app/email.py
+++ app/email.py
@@ -4,6 +4,7 @@ from app import mail
 from threading import Thread
 
 def send_async_email(app, msg):
+	current_app.logger.info("send_async email...")
 	with app.app_context():
 		mail.send(msg)
 
@@ -12,5 +13,6 @@ def send_email(subject, sender, recipients, text_body, html_body):
 	msg.body = text_body
 	msg.html = html_body
 	#mail.send(msg)
+	current_app.logger.info("send email...")
 	Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
 
