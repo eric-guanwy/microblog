df8bc7b694352aadb0398f16b9c5ce3a54bdf97c
eric-guanwy <guanwy01@gmail.com>
2018-08-06 21:19:44 +0800

    Chapter 23 DONE

--- app/api/users.py
+++ app/api/users.py
@@ -3,12 +3,15 @@ from app import db
 from app.api import bp
 from app.models import User
 from app.api.errors import bad_request
+from app.api.auth import token_auth
 
 @bp.route('/users/<int:id>', methods=['GET'])
+@token_auth.login_required
 def get_user(id):
 	return jsonify(User.query.get_or_404(id).to_dict())
 
 @bp.route('/users', methods=['GET'])
+@token_auth.login_required
 def get_users():
 	page = request.args.get('page',1,type=int)
 	per_page = request.args.get('per_page', 10, type=int)
@@ -16,6 +19,7 @@ def get_users():
 	return jsonify(data)
 
 @bp.route('/users/<int:id>/followers', methods=['GET'])
+@token_auth.login_required
 def get_followers(id):
 	user = User.query.get_or_404(id)
 	page = request.args.get('page',1,type=int)
@@ -24,6 +28,7 @@ def get_followers(id):
 	return jsonify(data)
 
 @bp.route('/users/<int:id>/followed', methods=['GET'])
+@token_auth.login_required
 def get_followed(id):
 	user = User.query.get_or_404(id)
 	page = request.args.get('page',1,type=int)
@@ -52,6 +57,7 @@ def create_user():
 
 
 @bp.route('/users/<int:id>', methods=['PUT'])
+@token_auth.login_required
 def update_user(id):
 	user = User.query.get_or_404(id)
 	data = request.get_json() or {}
